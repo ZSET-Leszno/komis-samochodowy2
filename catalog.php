5f76bddc44281ed9a7850352bfc7dc021b472a7d
Rybook <m.manel@zset.leszno.pl>
2023-03-16 20:51:50 +0100

    tymczasowe dodanie conn.php

--- catalog.php
+++ catalog.php
@@ -1,3 +1,11 @@
 <?php
+    session_start();
+    require_once("E:\xamp\htdocs\Projekt_strona\komis-samochodowy2-2\conn.php");
 
+    $con = @new mysqli($host, $db_user, $db_password, $db_name);
+    if(!$con){
+        echo("nie działa");
+    } else{
+        echo("działa");
+    }
 ?>
