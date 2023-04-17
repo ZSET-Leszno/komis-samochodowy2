e0471cf83387e50eb752d2e29281fe3523159bba
Rybook <m.manel@zset.leszno.pl>
2023-04-17 20:09:49 +0200

    ogloszenie

--- ogloszenie.php
+++ ogloszenie.php
@@ -1,7 +1,6 @@
 <?php
     session_start();
     require_once("conn.php");
-    echo $_GET['ogl_id'];  
 ?>
 
 <!DOCTYPE html>
@@ -20,17 +19,10 @@
     <script async src="claser.js"></script>
     <title>Document</title>
     <script src="https://www.momentcrm.com/embed%22%3E"></script>
-    <script>
-        MomentCRM('init', {
-            'teamVanityId': 'caro',
-            'doChat': true,
-            'doTracking': true,
-        });
-    </script>
 </head>
 <body style="background-color: #201c2b;">
-    <!-- menu -->
-    <div id="sidenav">
+<!-- menu -->
+<div id="sidenav">
         <a href="index.html" id="Home" class="active" onclick="claser()">Home</a>
         <a href="#info" id="About" onclick="claser()">About</a>
         <a href="index.html">Contact</a>
@@ -59,65 +51,102 @@
             </div>
         </span>
     </nav>
+    <?php
 
-    <section id="Zdjecia_main">
-        <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel" style="margin-bottom:1rem ;">
-            <div class="carousel-inner">
-              <div class="carousel-item active" data-bs-interval="10000">
-                <img src="wp2619698.webp" class="d-block w-100" alt="...">
-              </div>
-              <div class="carousel-item" data-bs-interval="2000">
-                <img src="images.jpg" class="d-block w-100" alt="...">
-          
-              </div>
-              <div class="carousel-item">
-                <img src="breaking-bad-2008.jpg" class="d-block w-100" alt="...">
-              </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev" style="border:0; background-color: rgba(255, 255, 255, 0.247);">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="visually-hidden"></span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next" style="border:0; background-color: rgba(255, 255, 255, 0.247);">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="visually-hidden"></span>
-            </button>
-        </div>
-    </section>
-    <section id="Specyfikacja">
-        <h1>Model: Marka:</h1>
-        <div id="Spec_fiter">
-            <div>
-                <p>Moc:</p>
-                <p>Moc:</p>
-                <p>Moc:</p>
-                <p>Moc:</p>
-                <p>Moc:</p>
-            </div>
+        $con = mysqli_connect($host, $db_user, $db_password, $db_name);
+        $marka = "SELECT * FROM paliwa";
+        $query = mysqli_query($con, $marka);
+        while ($auto = mysqli_fetch_assoc($query)) {
             
-            <div>
-                <p>400KM</p>
-                <p>alalalalalalalalalalalalalaal</p>
-                <p>Moc:</p>
-                <p>Moc:</p>
-                <p>Moc:</p>
-            </div>
-        </div>
-    </section>
-    <section id="Specyfikacja_2">
-        <h1>Opis:</h1>
-        <div id="Spec_fiter">
-            <div>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum, veniam quam repellat illo perferendis, in doloremque, tempora blanditiis pariatur ut dolorum vero voluptatibus natus quibusdam voluptate nostrum neque id quas!Lorem</p>
-            </div>
-    </section>
-    <div id="Zakup">
-        <h1>Cena: 555555 PLN</h1>
-        <a href=""><button>Zamów</button></a>
-    </div>
-    </section>
-    <footer>
+        }
+            mysqli_close($con);
         
+
+
+        $con = mysqli_connect($host, $db_user, $db_password, $db_name);
+        $marka = "SELECT * FROM kolory";
+        $query = mysqli_query($con, $marka);
+        while ($auto = mysqli_fetch_assoc($query)) {
+        }
+            mysqli_close($con);
+        ?>
+    <?php
+    
+        $con = mysqli_connect($host, $db_user, $db_password, $db_name);
+        $zapytanie = "SELECT * FROM samochody JOIN modele ON samochody.Id_model = modele.Id JOIN paliwa ON samochody.Id_paliwo = paliwa.Id JOIN marki ON modele.Id_marki = marki.Id WHERE Id_s =" .$_GET['ogl_id'];
+        $wynik = mysqli_query($con,$zapytanie);
+        $auto = mysqli_fetch_assoc($wynik);
+    
+    
+        echo'
+            <section id="Zdjecia_main">
+                <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel" style="margin-bottom:1rem ;">
+                    <div class="carousel-inner">
+                    <div class="carousel-item active" data-bs-interval="10000">
+                        <img src="'.$auto['zdj_1'].'" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item" data-bs-interval="2000">
+                        <img src="'.$auto['zdj_2'].'" class="d-block w-100" alt="...">
+                
+                    </div>
+                    <div class="carousel-item">
+                        <img src="'.$auto['zdj_3'].'" class="d-block w-100" alt="...">
+                    </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev" style="border:0; background-color: rgba(255, 255, 255, 0.247);">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden"></span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next" style="border:0; background-color: rgba(255, 255, 255, 0.247);">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden"></span>
+                    </button>
+                </div>
+            </section>';
+        echo'
+            <section id="Specyfikacja">
+                <h1>'.$auto['Nazwa_marki'].'</h1>
+                <div id="Spec_fiter">
+                    <div>
+                        <p>Moc:</p>
+                        <p>Pojemnosc skokowa cm3:</p>
+                        <p>Przebieg km:</p>
+                        <p>Paliwo:</p>
+                        <p>Rok produkcji:</p>
+                        <p>Liczba drzwi:</p>
+                    </div>
+                    
+                    <div>
+                        <p>'.$auto['Moc_KM'].' KM</p>
+                        <p>'.$auto['Pojemnosc_skokowa_cm3'].'</p>
+                        <p>'.$auto['Przebieg_km'].'</p>
+                        <p>'.$auto['Rodzaj_paliwa'].'</p>
+                        <p>'.$auto['Rok_produkcji'].'</p>
+                        <p>'.$auto['Liczba_drzwi'].'</p>
+                    </div>
+                </div>
+            </section>
+            <section id="Specyfikacja_2">
+                <h1>Opis:</h1>
+                <div id="Spec_fiter">
+                    <div>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum, veniam quam repellat illo perferendis, in doloremque, tempora blanditiis pariatur ut dolorum vero voluptatibus natus quibusdam voluptate nostrum neque id quas!Lorem</p>
+                    </div>
+            </section>
+            <div id="Zakup">
+                <h1>Cena: 555555 PLN</h1>
+                <a href=""><button>Zamów</button></a>
+            </div>
+            </section>';
+    ?>
+    <footer>
+    <script>
+        MomentCRM('init', {
+            'teamVanityId': 'caro',
+            'doChat': true,
+            'doTracking': true,
+        });
+    </script>
     </footer>
 </body>
 </html>
